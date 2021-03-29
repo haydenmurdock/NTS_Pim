f265a524a33ea0fc1afe0f953df77f389cf6062d
Hayden Murdock <hayden@nts.taxi>
2021-03-28 20:21:54 -0600

    working on the enter destination XML

--- app/src/main/java/com/example/nts_pim/fragments_viewmodel/enter_destination/EnterDestination.kt
+++ app/src/main/java/com/example/nts_pim/fragments_viewmodel/enter_destination/EnterDestination.kt
@@ -1,11 +1,17 @@
 package com.example.nts_pim.fragments_viewmodel.enter_destination
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.example.nts_pim.R
 import com.example.nts_pim.fragments_viewmodel.base.ScopedFragment
+import com.example.nts_pim.utilities.logging_service.LoggerHelper
+import com.example.nts_pim.utilities.view_helper.ViewHelper
+import kotlinx.android.synthetic.main.enter_destination_screen.*
+import kotlinx.android.synthetic.main.receipt_information_email.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 
@@ -22,5 +28,19 @@ class EnterDestination : ScopedFragment(), KodeinAware {
         super.onViewCreated(view, savedInstanceState)
 
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showSoftKeyboard()
+    }
+
+    private fun showSoftKeyboard(){
+        val imm =
+                requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(editTextTextPostalAddress, InputMethodManager.SHOW_IMPLICIT)
+            editTextTextPostalAddress.requestFocus()
+            ViewHelper.hideSystemUI(requireActivity())
+            LoggerHelper.writeToLog( "Showing Keyboard on enter destination fragment", null)
     }
 }
